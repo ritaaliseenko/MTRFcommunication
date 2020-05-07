39839a422b3fe94cbcb4c15d0cfab6495fc01cd3
Margarita <rita.aliseenko@gmail.com>
2020-05-07 13:57:46 +0300

    fix(main) #6 [Стильный код]

--- GoToBasicMode.py
+++ GoToBasicMode.py
@@ -3,8 +3,8 @@ import serial
 port = serial.Serial(port='COM3', baudrate=9600, stopbits=serial.STOPBITS_ONE, bytesize=serial.EIGHTBITS)
 # Переход из режима обновления ПО (занимает 12 секунд) в основной режим работы
 # Кажется, у меня это не срабатывает...
-basicModeCommand = bytearray([171, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 172])
-port.write(basicModeCommand)
+basic_mode_command = bytearray([171, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 172])
+port.write(basic_mode_command)
 
 answer_title = ['ST', 'MODE', 'CTR', 'TOGL', 'CH', 'CMD', 'FMT', 'D0', 'D1', 'D2', 'D3', 'ID0', 'ID1', 'ID2',
                'ID3', 'CRC', 'SP']
